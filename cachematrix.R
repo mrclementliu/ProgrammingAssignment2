37157353ea5c023312c250ecb41b9cc6cabbe202
mrclementliu <mrclementliu@gmail.com>
2015-05-22 16:27:53 -0700

    submitting homework!

--- cachematrix.R
+++ cachematrix.R
@@ -2,31 +2,6 @@
 
 # makeCacheMatrix creates a matrix that can cache its inverse
 
-makeVector <- function(x = numeric()) {
-	m <- NULL
-	set <- function(y) {
-		x <<- y
-		m <<- NULL
-	}
-	get <- function() x
-	setmean <- function(mean) m <<- mean
-	getmean <- function() m
-	list(set = set, get = get, setmean = setmean, getmean = getmean)
-}
-
-cachemean <- function(x, ...) {
-	m <- x$getmean()
-	if(!is.null(m)) {
-		message("getting cached data")
-		return(m)
-	}
-	data <- x$get()
-	m <- mean(data, ...)
-	x$setmean(m)
-	m
-}
-
-
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
 	set <- function(y) {
